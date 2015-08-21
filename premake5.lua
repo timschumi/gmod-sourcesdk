7eb56b455f07a3ff187a75670bb7aeec8b2f3e47
William Wallace <me@wiox.me>
2015-08-21 14:16:07 +0100

    Proper 'defines' used by SDK to detect OS

--- premake5.lua
+++ premake5.lua
@@ -4,4 +4,11 @@ local libLocations = {
 	linux	= "src/lib/public/linux32"
 }
 
+local osDefinition = {
+	windows	= { "WIN32" },
+	macosx	= { "POSIX", "LINUX", "OSX" },
+	linux	= { "POSIX", "LINUX" }
+}
+
 libdirs(libLocations[os.get()])
+defines(osDefinition[os.get()])
